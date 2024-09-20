ceba4331b22a15f4c2e7cf8aa9e223875e6a0b92
223880 <rafael.segala45@gmail.com>
2024-09-20 11:32:52 -0300

    Fix

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const dns = require('dns');
-
+const BIP353 = require('bip353');
+class BIP353 {
+  constructor() {
+    this.dns = d
+    ns;
+  }
+}
 const resolveDNSPayment = (domain) => {
   return new Promise((resolve, reject) => {
     dns.resolveTxt(domain, (err, addresses) => {
