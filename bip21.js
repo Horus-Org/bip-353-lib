c0f34cd6571b200db4d93c3bd65608a569797f48
223880 <rafael.segala45@gmail.com>
2024-09-30 06:12:04 -0300

    Add BIP 21 library support

--- bip21.js
+++ bip21.js
@@ -7,3 +7,18 @@ const generateBIP21URI = (address, amount, label) => {
   
   const uri = generateBIP21URI('bitcoin_adress', '0.001', 'Payment for services');
   
+  const decode = (uri) => {
+    const url = new URL(uri);
+    return {
+      address: url.pathname.slice(1),
+      amount: url.searchParams.get('amount'),
+      label: url.searchParams.get('label'),
+    };
+  };
+
+  const encode = (address, amount, label) => {
+    const uri = new URL(`bitcoin:${address}`);
+    if (amount) uri.searchParams.set('amount', amount);
+    if (label) uri.searchParams.set('label', label);
+    return uri.toString();
+  };
